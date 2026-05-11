661af5fbc00f539c4d0df402ef8b11fb6887b9b4
Brian Herbert <brian@brianherbert.com>
2026-05-11 14:17:20 -0400

    chore: project scaffold

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+// src/constants.js
+export const CALL_TOPICS = [
+  'Debt reduction',
+  'Energy, solar, or utilities',
+  'Home improvement',
+  'Insurance',
+  'Investment related',
+  'Medical and prescriptions',
+  'Political',
+  'Prizes and sweepstakes',
+  'Reducing your interest rate',
+  'Student loans and grants',
+  'Travel and vacations',
+  'Warranties & protection plans',
+  'Other',
+];
